5210f5abb2ab28f68f465726343d3d6991c5d309
detailnur <detailnur@gmail.com>
2021-03-05 23:37:35 +0300

    main.java added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double mass=70;
+        double height=180;
+        double index=mass/((height/100)*(height/100));
+
+        System.out.println(index);
+    }
+}
